64cc88d970493483bc85914b1ac5faf392724317
Nguyễn Gia Phong <mcsinyx@disroot.org>
2021-04-19 09:43:22 +0700

    Perform some cosmetic changes

--- src/ulake_core/core.clj
+++ src/ulake_core/core.clj
@@ -1,29 +1,29 @@
 (ns ulake-core.core
   (:gen-class)
-  (:require [ulake-core.service])
+  (:require [ulake-core.grpc])
   (:import (com.rethinkdb RethinkDB)
-           (io.grpc Server ServerBuilder)
+           (io.grpc ServerBuilder)
            (io.grpc.stub StreamObserver)
-           (ulake-core.service GreeterServiceImpl)))
+           (ulake-core.grpc GreeterServiceImpl)))
 
 (def SERVER_PORT 50051)
-
-(defn start []
-  (let [greeter-service (new GreeterServiceImpl)
-        server (-> (ServerBuilder/forPort SERVER_PORT)
-                   (.addService greeter-service)
-                   (.build)
-                   (.start))]
-    (-> (Runtime/getRuntime)
-        (.addShutdownHook (Thread. #(if (not (nil? server))
-                                      (.shutdown server)))))
-    (if (not (nil? server))
-      (.awaitTermination server))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (with-open [conn (-> (.connection RethinkDB/r) .connect)]
-    (-> (.range RethinkDB/r 10) (.coerceTo "array") (.run conn) println))
+  (with-open [conn (.connect (.connection RethinkDB/r))]
+    (-> (.range RethinkDB/r 10)
+        (.coerceTo "array")
+        (.run conn)
+        println))
   (print "Now listening on port " SERVER_PORT)
-  (start))
+  (let [greeter-service (GreeterServiceImpl.)
+        server (-> (ServerBuilder/forPort SERVER_PORT)
+                   (.addService greeter-service)
+                   (.build)
+                   (.start))]
+    (.addShutdownHook (Runtime/getRuntime)
+                      (Thread. #(if (not (nil? server))
+                                  (.shutdown server))))
+    (if (not (nil? server))
+      (.awaitTermination server))))
