fc91ff2656fab5d8c3930966cfced908d69019c8
Nguyễn Gia Phong <mcsinyx@disroot.org>
2021-04-20 12:36:49 +0700

    Integrate with IPFS

--- project.clj
+++ project.clj
@@ -3,10 +3,11 @@
   :url "https://git.sr.ht/~cnx/ulake-core"
   :license {:name "GNU Affero General Public License v3.0"
             :url "https://www.gnu.org/licenses/agpl-3.0"}
+  :repositories [["jitpack" "https://jitpack.io"]]
   :dependencies [[org.clojure/clojure "1.10.3"]
+                 [aleph "0.4.6"]
+                 [com.github.ipfs/java-ipfs-http-client "1.3.3"]
                  [com.rethinkdb/rethinkdb-driver "2.4.4"]
-                 [compojure "1.6.2"]
-                 [http-kit "2.3.0"]
                  [ring/ring-devel "1.9.2"]
                  [ring/ring-jetty-adapter "1.9.2"]]
   :main ^:skip-aot ulake-core.core
--- src/ulake_core/core.clj
+++ src/ulake_core/core.clj
@@ -1,19 +1,29 @@
 (ns ulake-core.core
   (:gen-class)
-  (:require [compojure.core :refer [GET POST defroutes]]
-            [compojure.handler :refer [site]]
-            [org.httpkit.server :refer [run-server]]
+  (:require [aleph.http :refer [start-server]]
             [ring.middleware.reload :refer [wrap-reload]])
-  (:import (com.rethinkdb RethinkDB)))
+  (:import (com.rethinkdb RethinkDB)
+           (io.ipfs.api IPFS NamedStreamable$InputStreamWrapper)))
 
 (def in-dev? (constantly true))
+(def ipfs (IPFS. "/ip4/127.0.0.1/tcp/5001"))
 
-(defroutes all-routes
-  (POST "/ingest" [] #(.read (:body %))))
+(defn ipfs-add
+  "Add the content of the given stream to IPFS and return the CID."
+  [istream]
+  (-> (->> istream NamedStreamable$InputStreamWrapper. (.add ipfs))
+      (.get 0) .-hash str))
+
+(defn route [request]
+  (case (list (:request-method request) (:uri request))
+    ((:post "/echo")) {:status 200
+                     :body (ipfs-add (:body request))}
+    {:status 400
+     :body "unsupported"}))
 
 (defn -main [& args]
-  (let [handler (if (in-dev? args)
-                  (wrap-reload (site #'all-routes))
-                  (site all-routes))]
-    (run-server handler {:port 8090
-                         :max-bytes 4294967295})))
+  (start-server
+    (if (in-dev? args)
+      (wrap-reload #'route)
+      route)
+    {:port 8090}))
