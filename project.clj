a15dfe1c54ad1c531e36d7720bd71a3d90afd009
Nguyễn Gia Phong <mcsinyx@disroot.org>
2021-04-22 21:28:40 +0700

    Proxy Rethink filter query

--- project.clj
+++ project.clj
@@ -4,10 +4,11 @@
   :license {:name "GNU Affero General Public License v3.0"
             :url "https://www.gnu.org/licenses/agpl-3.0"}
   :repositories [["jitpack" "https://jitpack.io"]]
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [aleph "0.4.6"]
+  :dependencies [[aleph "0.4.6"]
                  [com.apa512/rethinkdb "1.0.0-SNAPSHOT"]
                  [com.github.ipfs/java-ipfs-http-client "1.3.3"]
+                 [org.clojure/clojure "1.10.3"]
+                 [org.clojure/data.json "2.2.1"]
                  [ring/ring-devel "1.9.2"]
                  [ring/ring-jetty-adapter "1.9.2"]]
   :main ^:skip-aot comlake-core.main
--- src/comlake_core/main.clj
+++ src/comlake_core/main.clj
@@ -18,6 +18,8 @@
 (ns comlake-core.main
   (:gen-class)
   (:require [aleph.http :refer [start-server]]
+            [clojure.data.json :as json]
+            [clojure.java.io :refer [reader]]
             [clojure.string :refer [starts-with?]]
             [comlake-core.ipfs :as ipfs]
             [rethinkdb.query :as r]
@@ -29,6 +31,7 @@
 (defn ingest
   "Ingest the data and return appropriate response."
   [request]
+  ;; FIXME: header should be checked before streaming file to IPFS
   (let [cid (ipfs/add (:body request)) ; TODO: handle exceptions
         object (into {"cid" cid}
                      (for [[k v] (:headers request)
@@ -39,15 +42,51 @@
           (r/insert object)
           (r/run conn)))
     {:status 200
+     :headers {:content-type "text/plain"}
      :body cid}))
+
+(defn parse
+  "Parse query into ReQL."
+  [row query]
+  (assert (map? query))
+  (assert (= (count query) 1))
+  (let [[k v] (first query)]
+    (case k ; FIXME: fallback properly
+      ("&" "|") (do (assert (vector? v))
+                    (apply (get {"&" r/and "|" r/or} k)
+                           (map (partial parse row) v)))
+      ("=") (do (assert (map? v))
+                (assert (= (count query) 1))
+                (let [op (get {"=" r/eq} k)
+                      [lhs rhs] (first v)]
+                  (op (r/get-field row lhs) rhs))))))
+
+(defn search
+  "Return query result as a HTTP response."
+  [request]
+  (try {:status 200
+        :headers {:content-type "application/json"}
+        :body (json/write-str
+                (with-open [conn (r/connect :host "127.0.0.1"
+                                            :port 28015
+                                            :db "test")]
+                  (-> (r/table "comlake")
+                      (r/filter (r/fn [row]
+                                  (parse row
+                                         (json/read (reader (:body request))))))
+                      (r/run conn))))}
+       (catch AssertionError e {:status 400
+                                :body "malformed query"})))
 
 (defn route
   "Route HTTP endpoints."
   [request]
-  (case (list (:request-method request) (:uri request))
-    ((:post "/ingest")) (ingest request)
+  (case [(:request-method request) (:uri request)]
+    [:post "/ingest"] (ingest request)
+    [:post "/filter"] (search request)
     {:status 400
-     :body "unsupported"}))
+     :headers {:content-type "text/plain"}
+     :body "unsupported\n"}))
 
 (defn -main [& args]
   ;; TODO: Abstract this away
