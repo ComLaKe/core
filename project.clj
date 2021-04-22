21e7fed23f5d0cfd9fee580ab5493cfd7f9a0988
Nguyễn Gia Phong <mcsinyx@disroot.org>
2021-04-22 10:53:32 +0700

    Get ingestion working

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
   :repositories [["jitpack" "https://jitpack.io"]]
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [aleph "0.4.6"]
+                 [com.apa512/rethinkdb "1.0.0-SNAPSHOT"]
                  [com.github.ipfs/java-ipfs-http-client "1.3.3"]
-                 [com.rethinkdb/rethinkdb-driver "2.4.4"]
                  [ring/ring-devel "1.9.2"]
                  [ring/ring-jetty-adapter "1.9.2"]]
   :main ^:skip-aot ulake-core.core
--- src/ulake_core/core.clj
+++ src/ulake_core/core.clj
@@ -1,11 +1,13 @@
 (ns ulake-core.core
   (:gen-class)
   (:require [aleph.http :refer [start-server]]
+            [clojure.string :refer [starts-with?]]
+            [rethinkdb.query :as r]
             [ring.middleware.reload :refer [wrap-reload]])
-  (:import (com.rethinkdb RethinkDB)
-           (io.ipfs.api IPFS NamedStreamable$InputStreamWrapper)))
+  (:import (io.ipfs.api IPFS NamedStreamable$InputStreamWrapper)))
 
 (def in-dev? (constantly true))
+(def header-prefix "content-")
 (def ipfs (IPFS. "/ip4/127.0.0.1/tcp/5001"))
 
 (defn ipfs-add
@@ -14,14 +16,34 @@
   (-> (->> istream NamedStreamable$InputStreamWrapper. (.add ipfs))
       (.get 0) .-hash str))
 
-(defn route [request]
+(defn ingest
+  "Ingest the data and return appropriate response."
+  [request]
+  (let [cid (ipfs-add (:body request)) ; TODO: handle exceptions
+        object (into {"cid" cid}
+                     (for [[k v] (:headers request)
+                           :when (starts-with? k header-prefix)]
+                       [(subs k (count header-prefix)) v]))]
+    (with-open [conn (r/connect :host "127.0.0.1" :port 28015 :db "test")]
+      (-> (r/table "ulake")
+          (r/insert object)
+          (r/run conn)))
+    {:status 200
+     :body (str object \space cid \newline)}))
+
+(defn route
+  "Route HTTP endpoints."
+  [request]
   (case (list (:request-method request) (:uri request))
-    ((:post "/echo")) {:status 200
-                     :body (ipfs-add (:body request))}
+    ((:post "/ingest")) (ingest request)
     {:status 400
      :body "unsupported"}))
 
 (defn -main [& args]
+  (with-open [conn (r/connect :host "127.0.0.1" :port 28015 :db "test")]
+    (when (some #{"ulake"} (r/run (r/table-list) conn))
+      (r/run (r/table-drop "ulake") conn))
+    (r/run (r/table-create "ulake") conn))
   (start-server
     (if (in-dev? args)
       (wrap-reload #'route)
