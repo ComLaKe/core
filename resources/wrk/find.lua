1826a52fcae857200030e238620d802cfc669732
Nguyễn Gia Phong <mcsinyx@disroot.org>
2021-05-10 12:11:09 +0700

    Add wrk scripts for convenient benchmarking

---
+++ resources/wrk/find.lua
@@ -0,0 +1,4 @@
+wrk.method = 'POST'
+wrk.path = '/find'
+wrk.headers['Content-Type'] = 'application/json'
+wrk.body = '["==", [".", "cid"], "Qm"]'
---
+++ resources/wrk/get.lua
@@ -0,0 +1,2 @@
+wrk.method = 'GET'
+wrk.path = '/get/QmbwXK2Wg6npoAusr9MkSduuAViS6dxEQBNzqoixanVtj5'
