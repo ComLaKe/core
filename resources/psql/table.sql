b4dcdcb3b7d43c7a97d693da7d177b7071521127
Nguyễn Gia Phong <mcsinyx@disroot.org>
2021-06-14 10:14:45 +0700

    Begin spliting metadata into two tables

--- resources/psql/table.sql
+++ resources/psql/table.sql
@@ -7,3 +7,18 @@ CREATE TABLE comlake (id serial primary key,
                       source text,
                       topics text[],
                       optional json);
+DROP TABLE IF EXISTS files;
+CREATE TABLE files (id bigserial PRIMARY KEY,
+                    cid text,
+                    length bigint,
+                    type text,
+                    name text,
+                    info json);
+DROP TABLE IF EXISTS entries;
+CREATE TABLE entries (id bigserial PRIMARY KEY,
+                      parent bigint REFERENCES entries,
+                      file bigint REFERENCES files,
+                      description text,
+                      source text,
+                      topics text[],
+                      optional json);
