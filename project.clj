734bc06740010a99c08db1f5f35b2889737ccd33
Nguyễn Gia Phong <mcsinyx@disroot.org>
2021-06-18 21:47:32 +0700

    Release 0.3.0
    Resolve GH-4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject comlake.core "0.3.0-SNAPSHOT"
+(defproject comlake.core "0.3.0"
   :description "USTH data lake core"
   :url "https://github.com/ComLake/comlake.core"
   :license {:name "GNU Affero General Public License 3.0"
